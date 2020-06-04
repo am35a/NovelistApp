a61cc0b1e3c3124da293d0a9d978bc173f55c399
Arthur A. Selimov <am35a.piston@gmail.com>
2020-06-04 23:44:26 +0300

    built share book component

--- src/App.vue
+++ src/App.vue
@@ -82,8 +82,8 @@
                         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
                     </svg>
                 </div>
-                <div>
-                    ...
+                <div class="text-center align-self-center">
+                    <ShareBook></ShareBook>
                 </div>
             </section>
             <section v-if="isSectionRate" class="player rate">
@@ -144,6 +144,7 @@
     import SectionList      from './components/SectionList.vue'
     import SectionMenu      from './components/SectionMenu.vue'
 
+    import ShareBook        from './components/ShareBook.vue'
     import FiveStarRate     from './components/FiveStarRate.vue'
 
     
@@ -159,6 +160,7 @@
             SectionList,
             SectionMenu,
 
+            ShareBook,
             FiveStarRate
         },
         data() {
