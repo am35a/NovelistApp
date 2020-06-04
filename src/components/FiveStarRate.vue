974d1f9ee50a652229b069fa7348a9a56a798b08
Arthur A. Selimov <am35a.piston@gmail.com>
2020-06-04 22:26:00 +0300

    rebuld of five star vote

--- src/components/FiveStarRate.vue
+++ src/components/FiveStarRate.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="rate">
-        <svg v-for="(star, index) in stars" :key="index" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <svg v-for="(star, index) in stars" :key="index" @click="vote(index + 1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <polygon :class="[(index + 1) > $parent.playBook.vote ? 'opa-20' : '']" points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"/>
         </svg>
-        <input @click="toggleRate" class="opa-0" v-model="$parent.playBook.vote" type="range" min="1" max="5" step="1">
     </div>
 </template>
 
@@ -16,15 +15,8 @@
             }
         },
         methods: {
-            toggleRate(){
-                switch(this.$parent.playBook.vote) {
-                case "0":
-                    this.$parent.playBook.vote = "1"
-                    break
-                case "1":
-                    this.$parent.playBook.vote = "0"
-                    break
-                }
+            vote(index){
+                index === this.$parent.playBook.vote && index === 1 ? this.$parent.playBook.vote = 0 : this.$parent.playBook.vote = index
             }
         }
     }
