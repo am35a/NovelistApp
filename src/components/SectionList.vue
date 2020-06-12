ac73c7d6a7cde08a5c3ccdf5463e1aa42468370f
Arthur A. Selimov <am35a.piston@gmail.com>
2020-06-12 23:21:14 +0300

    added hiding or showing player

--- src/components/SectionList.vue
+++ src/components/SectionList.vue
@@ -3,7 +3,7 @@
         <div class="item" v-for="(book, index) in $parent.sortBooksList" :key="index" @click="$parent.playerOpen(index)"> <!--@click="playerOpen(index)"--> <!-- :class="[showPlayer == book.id ? 'opa-40' : '']"-->
             <img class="thumb rounded-lg shadow-sm" :src="`http://mobitoon.ru/novelist/images/books/${book.id}/preview.jpg`" :alt="`${book.title}`">
             <div class="title text-truncate">{{ book.title }}</div>
-            <div class="author opa-40">{{ book.author }}</div>
+            <small class="author opa-40">{{ book.author }}</small>
             <div class="status" :class="[ completed(book.chapters) != 100 ? 'bg-warning rounded-left' : 'bg-success rounded' ]" :style="{ width: `${completed(book.chapters)}%` }"></div>
             <div class="status text-center">{{ completed(book.chapters) }}%</div>
         </div>
