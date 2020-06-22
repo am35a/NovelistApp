f4dc9d01b8bcc60f77b48fe17bc642423d7d97e9
Arthur A. Selimov <am35a.piston@gmail.com>
2020-06-22 09:42:31 +0300

    improved filter section logic

--- src/components/SectionNovelist.vue
+++ src/components/SectionNovelist.vue
@@ -2,7 +2,7 @@
     <section>
         <img src="http://mobitoon.ru/novelist/images/logo/logoname.svg" alt="Novelist">
         <div class="pt-3 text-center">
-            <span class="m-2">&copy; 2020</span>
+            <span class="m-2">&copy;2020</span>
             <a class="m-2 text-uppercase text-decoration-none text-primary" href="http://noveli.st" target="_blank">www.noveli.st</a>
             <span class="m-2">TX USA</span>
         </div>
