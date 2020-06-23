65055e0ac4fa290d26439cf9418d957f04ffd82a
Arthur A. Selimov <am35a.piston@gmail.com>
2020-06-23 11:44:38 +0300

    Hide "play" button

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@
                     </svg>
                     <svg v-else class="part-prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
                     <div class="my-auto text-center">
-                        {{ playChapter.title }}
+                        {{ playChapter.title ? playChapter.title : playBook.title }}
                     </div>
                     <svg v-if="playBookPartNextDisable" @click="palyerSelectChapter(1)" class="part-next" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M6 18l8.5-6L6 6v12zM16 6v12h2V6h-2z"/>
@@ -70,12 +70,17 @@
                         <path d="M11 18V6l-8.5 6 8.5 6zm.5-6l8.5 6V6l-8.5 6z"/>
                     </svg>
                     <svg v-else class="part-prev" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
-                    <svg v-if="playStartDisable" @click="playerPause" class="pause" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
-                    </svg>
-                    <svg v-else @click="playerStart" class="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M8 5v14l11-7z"/>
-                    </svg>
+
+                    <template v-if="playerPlayPauseDisabled">
+                        <svg v-if="playStartDisable" @click="playerPause" class="pause" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+                        </svg>
+                        <svg v-else @click="playerStart" class="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M8 5v14l11-7z"/>
+                        </svg>
+                    </template>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
+
                     <svg v-if="playRewindFrontDisable" class="rewind-front my-auto" @click="playerRewind(1)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M4 18l8.5-6L4 6v12zm9-12v12l8.5-6L13 6z"/>
                     </svg>
@@ -384,6 +389,9 @@
             },
             playStartDisable() {
                 return this.isPlayerStarted
+            },
+            playerPlayPauseDisabled() {
+                return this.playRewindFrontDisable
             }
 
         },
